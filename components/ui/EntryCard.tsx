b525e83f8f4c7c4e6f6db0142f462dac9775e8ec
Esteban Canteros <ecanteros.churrasco@wallypos.com>
2022-07-06 15:51:25 -0300

    enabling drag & drop

--- components/ui/EntryCard.tsx
+++ components/ui/EntryCard.tsx
@@ -1,19 +1,41 @@
-import { Card, CardActionArea, CardActions, CardContent, Typography } from '@mui/material'
-import { Entry } from '../../interfaces'
+import { DragEvent } from "react"
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@mui/material"
+import { Entry } from "../../interfaces"
 
 interface Props {
   entry: Entry
 }
 
 const EntryCard = ({ entry }: Props) => {
+  const onDragStart = (event: DragEvent) => {
+    event.dataTransfer.setData("text", entry._id)
+  }
+
+  const onDragEnd = () => {}
+
   return (
-    <Card style={{ marginBottom: 1 }}>
+    <Card
+      style={{ marginBottom: 1 }}
+      draggable
+      onDragStart={onDragStart}
+      onDragEnd={onDragEnd}
+    >
       <CardActionArea>
         <CardContent>
-          <Typography sx={{ whiteSpace: 'pre-line' }}>{entry.description}</Typography>
+          <Typography sx={{ whiteSpace: "pre-line" }}>
+            {entry.description}
+          </Typography>
         </CardContent>
-        <CardActions sx={{ display: 'flex', justifyContent: 'end', paddingRight: 2 }}>
-          <Typography variant='body2'>30 min ago</Typography>
+        <CardActions
+          sx={{ display: "flex", justifyContent: "end", paddingRight: 2 }}
+        >
+          <Typography variant="body2">30 min ago</Typography>
         </CardActions>
       </CardActionArea>
     </Card>
--- components/ui/EntryList.tsx
+++ components/ui/EntryList.tsx
@@ -1,8 +1,8 @@
-import { useContext, useMemo } from 'react'
-import { List, Paper } from '@mui/material'
-import { EntriesContext } from '../../context/entries/'
-import { EntryCard } from './'
-import { EntryStatus } from '../../interfaces'
+import { useContext, useMemo, DragEvent } from "react"
+import { List, Paper } from "@mui/material"
+import { EntriesContext } from "../../context/entries/"
+import { EntryCard } from "./"
+import { EntryStatus } from "../../interfaces"
 
 interface Props {
   status: EntryStatus
@@ -11,17 +11,29 @@ interface Props {
 const EntryList = ({ status }: Props) => {
   const { entries } = useContext(EntriesContext)
 
-  const entriesByStatus = useMemo(() => entries.filter((entry) => entry.status === status), [entries, status])
+  const entriesByStatus = useMemo(
+    () => entries.filter((entry) => entry.status === status),
+    [entries, status]
+  )
+
+  const allowDrop = (event: DragEvent) => {
+    event.preventDefault()
+  }
+
+  const onDropEntry = (event: DragEvent) => {
+    const id = event.dataTransfer.getData("text")
+    console.log(id)
+  }
 
   return (
-    <div>
+    <div onDrop={onDropEntry} onDragOver={allowDrop}>
       <Paper
         sx={{
-          height: 'calc(100vh - 180px)',
-          overflow: 'scroll',
-          padding: '1px 5px',
-          backgroundColor: 'transparent',
-          '&::-webkit-scrollbar': { display: 'none' },
+          height: "calc(100vh - 180px)",
+          overflow: "scroll",
+          padding: "1px 5px",
+          backgroundColor: "transparent",
+          "&::-webkit-scrollbar": { display: "none" },
         }}
       >
         <List sx={{ opacity: 1 }}>
