b1ead7cc5737d37abe277a9b8dcdd22046cc8818
Esteban Canteros <estebanacanteros@gmail.com>
2022-10-03 19:36:52 -0300

    connect to the database through env variables

--- database/db.ts
+++ database/db.ts
@@ -26,13 +26,13 @@ export const connect = async () => {
     await mongoose.disconnect()
   }
 
-  await mongoose.connect("......")
+  await mongoose.connect(process.env.MONGO_URL || '')
   mongoConnection.isConnected = 1
-  console.log("Connected to mongodb", " ")
+  console.log("Connected to mongodb", process.env.MONGO_URL)
 }
 
 export const disconnect = async () => {
-  if (mongoConnection.isConnected !== 0) return
+  if (mongoConnection.isConnected === 0) return
   await mongoose.disconnect()
   console.log("Disconnected from mongoose")
 }
--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,9 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '../../database'
+import { disconnect } from '../../database/db';
 
 type Data = {
-  name: string
+  message: string
 }
 
-export default function handler (req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ name: 'Example' })
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  if (process.env.NODE_ENV === 'production') {
+    return res.status(401).json({ message: 'You dont hace access to this service' })
+  }
+
+  await db.connect()
+
+  await db.disconnect()
+
+  res.status(200).json({ message: 'Process went sucessfully' })
 }
