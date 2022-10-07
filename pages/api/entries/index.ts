2c70e43c374a6ab931e5e4976490ae57ff6422a2
Esteban Canteros <estebanacanteros@gmail.com>
2022-10-07 14:23:38 -0300

    add endpoint for create an entry

--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -2,25 +2,53 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { db } from '../../../database'
 import EntryModel, { IEntry } from '../../../models/Entry'
 
-type Data = { message: string } | { entries: IEntry[] }
+type Data = { message: string } | { entries: IEntry[] } | { entry: IEntry }
 
 export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
   switch (req.method) {
     case 'GET':
       return getEntries(res)
-    //case 'POST':
-    //  return createEntries()
+    case 'POST':
+      return createEntry(req, res)
     default:
       return res.status(400).json({ message: 'endpoint doesnt exists' })
   }
 }
 
 const getEntries = async (res: NextApiResponse<Data>) => {
-  await db.connect()
+  try {
+    await db.connect()
 
-  const entries = await EntryModel.find().sort({ createdAt: 'ascending' })
+    const entries = await EntryModel.find().sort({ createdAt: 'ascending' })
 
-  await db.disconnect()
+    await db.disconnect()
 
-  res.status(200).json({ entries })
+    res.status(200).json({ entries })
+  } catch (error) {
+    await db.disconnect()
+    console.log(error)
+    return res.status(500)
+  }
+}
+
+//IMPORTANT body will only be automatically parsed if content-type is application/json or form x-www-form-urlencoded
+const createEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { description } = req.body
+
+  if (!description) return res.status(404).json({ message: "param 'description' should be provided" })
+
+  try {
+    await db.connect()
+    const entry = await EntryModel.create({
+      status: 'pending',
+      createdAt: Date.now(),
+      description,
+    })
+    await db.disconnect()
+    res.status(201).json({ entry })
+    await db.disconnect()
+  } catch (error) {
+    console.log(error)
+    return res.status(500)
+  }
 }
