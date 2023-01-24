8b5795ef1f985ab49051a1c7827aad552ae0f121
Esteban Canteros <estebanacanteros@gmail.com>
2023-01-24 20:39:53 -0300

    add endpoint to get entry by id

--- pages/api/entries/[id].ts
+++ pages/api/entries/[id].ts
@@ -13,6 +13,8 @@ export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
       return updateEntry(req, res, id)
     case 'DELETE':
       return deleteEntry(req, res, id)
+    case 'GET':
+      return getEntry(req, res, id)
     default:
       return res.status(400).json({ message: 'endpoint doesnt exists' })
   }
@@ -56,6 +58,23 @@ const deleteEntry = async (req: NextApiRequest, res: NextApiResponse<Data>, id: 
 
     await db.disconnect()
     return res.status(200).json({ message: 'entry deleted' })
+  } catch (error) {
+    console.log(error)
+    return res.status(500)
+  }
+}
+
+const getEntry = async (req: NextApiRequest, res: NextApiResponse<Data>, id: string) => {
+  try {
+    await db.connect()
+    const entry = await EntryModel.findById(id)
+
+    if (!entry) {
+      return res.status(404).json({ message: `There is no entry with id ${id}` })
+    }
+
+    await db.disconnect()
+    return res.status(200).json({ entry })
   } catch (error) {
     console.log(error)
     return res.status(500)
